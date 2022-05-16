14b57953bc250a7b76b6f90224f592e63f637f4b
Ariyos <ariyosbefikadu@gamil.com>
2022-05-16 17:00:16 -0400

    commit from eclipse

---
+++ Addis Ababa/src/sheger/cherkos.java
@@ -0,0 +1,14 @@
+package sheger;
+
+public class cherkos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("chercher godana");
+		System.out.println("yebolew godana");
+		System.out.println("habte giorgis deldey");
+		System.out.println("ayer tena menged");
+	}
+
+}
