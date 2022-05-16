77251fb1fead46f63c8f5936dbd3ea9cad68af14
Ariyos <ariyosbefikadu@gamil.com>
2022-05-16 16:27:25 -0400

    this is my a very first commit

---
+++ Yebu Mana/src/Jimma/Buture.java
@@ -0,0 +1,14 @@
+package Jimma;
+
+public class Buture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  
+		System.out.println("gebisa alemayehu mecha chat");
+		System.out.println("borena chat");
+		System.out.println("lalo and digo chat market");
+		System.out.println("bilida market of chat");
+	}
+
+}
